889a60b890598fe90246220fee4f9bdc890bc29b
chaswrig <42905430+chaswrig@users.noreply.github.com>
2024-05-16 13:18:03 -0500

    Computer Choice -- Computer's play logic implemented. Each option should have a one third chance.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("Hello world!")
+function getComputerChoice() {
+    //Return computer's choice of rock, paper, or scissors
+    let dice = Math.floor(Math.random() * 100);
+    let choice;
+    if (dice < 35) {
+        choice = "Rock";
+    } else if (dice >= 35 && dice < 68) {
+        choice = "Paper";
+    } else if (dice >= 68 ) {
+        choice = "Scissors";
+    }
+
+    return choice;
+}
+
+console.log(getComputerChoice());
