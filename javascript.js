29c7304b36f298fc4a6667538d2519f862bac474
chaswrig <42905430+chaswrig@users.noreply.github.com>
2024-05-16 13:38:23 -0500

    Human Choice -- Player's choice logic implemented. Future state, probably need to fix default case to call the function again.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,39 @@
 function getComputerChoice() {
     //Return computer's choice of rock, paper, or scissors
     let dice = Math.floor(Math.random() * 100);
-    let choice;
+    let computerChoice;
     if (dice < 35) {
-        choice = "Rock";
+        computerChoice = "Rock";
     } else if (dice >= 35 && dice < 68) {
-        choice = "Paper";
+        computerChoice = "Paper";
     } else if (dice >= 68 ) {
-        choice = "Scissors";
+        computerChoice = "Scissors";
     }
 
-    return choice;
+    return computerChoice;
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    let humanChoice;
+    let choice = prompt("Press 1 for Rock, 2 for Paper, or 3 for Scissors");
+    switch (choice) {
+        case "1":
+            humanChoice = "Rock";
+            break;
+
+        case "2":
+            humanChoice = "Paper";
+            break;
+        
+        case "3":
+            humanChoice = "Scissors";
+            break;
+        
+        default:
+            humanChoice = "Rock";
+    }
+
+    return humanChoice;
+}
+
+console.log(getHumanChoice());
